bf7e383ba8d3ebd57b98469de40ede0027bf35f6
prathamSrivastva <94467282+prathamSrivastva@users.noreply.github.com>
2023-09-25 21:23:29 +0000

    data_tpes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "pratham.srk1998@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+//accountId = 2 // not allowed
+
+
+accountEmail = "hc@gmail.com"
+accountPassword = "2121231"
+console.log(accountEmail);
+console.log(accountCity)
---
+++ .devcontainer/01_basics/02_variable.js
@@ -0,0 +1,20 @@
+//alert("hello") we are using node js not brwser
+
+
+let name = "hitesh"
+let age  = 18;
+let isLoggedIn =  false;
+
+
+
+// PRIMITIVE DATA TYPES
+// number = > 2^53 
+//bigint  = > very very big number
+//string = "";
+//booleon => han ya na
+//null => standalone value =>  jab kuch value nahi mili => iska typeof object --> why?
+//undefined => value abhi assign nahi hui   => iska typeof undefined
+//symbol => unique
+
+//NON PRIMITIVE
+//OBJECTS
